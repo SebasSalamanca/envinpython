94eb85bb55dc89224a9f69e76987da2c734ea5a3
elsebas <jsebastian1119@gmail.com>
2023-03-10 17:56:07 -0500

    Segunda versión totalemente funcional, se agregan archivos docker

--- primer.py
+++ primer.py
@@ -7,23 +7,25 @@ from folder.rorganice import count_level as lv
 from folder.game import input_user as inp
 from folder.game import play_game as gm
 from folder.rorganice import desicion as ds
-
+from folder.rorganice import one_time as one
 from folder.screen import images as img
 
 def run():
     #system('clear')
-    position,lives,levelflag = 0,5,0
-    listgame, listgameaux = [],[]
+    position,lives,levelflag,wordplayed = 0,5,0,'s'
+    listgame, listgameaux, deslista = [],[],[]
     wordlist = rd()
+    print(img(5, wordplayed))
     while lives > 0:
         wordplayed, position, lives = lv(levelflag,wordlist,position,lives)
+        one(wordplayed, listgame, deslista)
         user = inp(wordplayed)
         system('clear')
         listgame,listgameaux,lives = gm(levelflag,wordplayed,user,listgame,listgameaux,lives)
-        levelflag, listgameaux, lives = ds(levelflag, listgameaux, lives)           
-    if lives == 0:
+        levelflag, listgameaux, lives, deslista, rescue = ds(levelflag, listgameaux, lives, position)   
+    if lives == 0 and levelflag == 1 and rescue == 0:
         system('clear')
-        print(img(0))
+        print(img(0, wordplayed))
      
 if __name__ == '__main__':
     run()
